10feca3b5f942f8e0fd7fd73c434c05f6940155c
Glen <70684146+gwg313@users.noreply.github.com>
2023-11-08 14:10:04 -0500

    Update oil.nix

--- config/oil.nix
+++ config/oil.nix
@@ -5,22 +5,22 @@
   extraPlugins = with pkgs.vimPlugins; [nvim-web-devicons];
 
   plugins.oil.keymaps = {
-      "g?" = "actions.show_help";
-      "<CR>" = "actions.select";
-      "<C-s>" = "actions.select_vsplit";
-      "<C-h>" = "actions.select_split";
-      "<C-t>" = "actions.select_tab";
-      "<C-p>" = "actions.preview";
-      "<C-c>" = "actions.close";
-      "<C-l>" = "actions.refresh";
-      "-" = "actions.parent";
-      "_" = "actions.open_cwd";
-      "`" = "actions.cd";
-      "~" = "actions.tcd";
-      "gs" = "actions.change_sort";
-      "g." = "actions.toggle_hidden";
-    };
-    
+    "g?" = "actions.show_help";
+    "<CR>" = "actions.select";
+    "<C-s>" = "actions.select_vsplit";
+    "<C-h>" = "actions.select_split";
+    "<C-t>" = "actions.select_tab";
+    "<C-p>" = "actions.preview";
+    "<C-c>" = "actions.close";
+    "<C-l>" = "actions.refresh";
+    "-" = "actions.parent";
+    "_" = "actions.open_cwd";
+    "`" = "actions.cd";
+    "~" = "actions.tcd";
+    "gs" = "actions.change_sort";
+    "g." = "actions.toggle_hidden";
+  };
+
   keymaps = [
     {
       mode = "n";
@@ -34,3 +34,4 @@
     }
   ];
 }
+
